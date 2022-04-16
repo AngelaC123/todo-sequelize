8737de75d6eb4f95dfcf84cd40fcf35d407de9b8
AngelaC123 <angelarare@gmail.com>
2022-04-16 15:04:38 +0800

    Add authenticator to home and todos routers

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const { authenticator } = require('../middleware/auth')
 
 const db = require('../models')
-
 const users = require('./modules/users')
 const home = require('./modules/home')
 const todos = require('./modules/todos')
 
 
-router.use('/', home)
 router.use('/users', users)
-router.use('/todos', todos)
+router.use('/todos', authenticator, todos)
+router.use('/', authenticator, home)
 
 module.exports = router
 
