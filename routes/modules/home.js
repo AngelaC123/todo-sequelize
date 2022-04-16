dd568c8704c4f93072e9838442de66babcd1fcb0
AngelaC123 <angelarare@gmail.com>
2022-04-16 23:10:43 +0800

    Modify home router to display login user's todo

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,14 +3,22 @@ const router = express.Router()
 
 const db = require('../../models')
 const Todo = db.Todo
+const User = db.User
 
 router.get('/', (req, res) => {
-  return Todo.findAll({
-    raw: true,
-    nest: true,
-  })
-    .then(todos => res.render('index', { todos: todos })
-    )
+
+  return User.findByPk(req.user.id)
+    .then((user) => {
+      if (!user) throw new Error('user not found')
+
+      return Todo.findAll({
+        raw: true,
+        next: true,
+        where: { UserId: req.user.id }
+      })
+    })
+
+    .then(todos => res.render('index', { todos: todos }))
     .catch(error => res.status(422).json(error))
 })
 
