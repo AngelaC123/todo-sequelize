a2148c218485c796b18e45e78fbb996699dfa456
AngelaC123 <angelarare@gmail.com>
2022-04-16 11:51:45 +0800

    Add detail page

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ const db = require('../models')
 
 const users = require('./modules/users')
 const home = require('./modules/home')
+const todos = require('./modules/todos')
 
 
-router.use('/',home)
+router.use('/', home)
 router.use('/users', users)
+router.use('/todos', todos)
 
 module.exports = router
 
