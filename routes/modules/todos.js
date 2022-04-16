eb74eed30f30c9e9abff536b406acabb3f55d5a8
AngelaC123 <angelarare@gmail.com>
2022-04-16 23:17:40 +0800

    Modify view detail feature

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -4,11 +4,18 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
+// view detail
 router.get('/:id', (req, res) => {
   const id = req.params.id
-  return Todo.findByPk(id)
+  const userId = req.user.id
+  return Todo.findOne({ where: { id, userId } })
     .then(todo => res.render('detail', { todo: todo.toJSON() }))
     .catch(error => console.log(error))
 })
+
+// edit
+
+// delete
+
 
 module.exports = router
