a9274fc80cbfb53aa8f250422317ea836ae4d46b
AngelaC123 <angelarare@gmail.com>
2022-04-17 00:19:09 +0800

    Add edit todo feature

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -14,6 +14,37 @@ router.get('/:id', (req, res) => {
 })
 
 // edit
+// view edit page
+router.get('/:id/edit', (req, res) => {
+  // get user id, todo id
+  // find data
+  //render edit apge
+  const id = req.params.id
+  const UserId = req.user.id
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => res.render('edit', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+// post edited data
+router.put('/:id', (req, res) => {
+  // get data from input
+  // save to the data
+
+  const id = req.params.id
+  const UserId = req.user.id
+  const { isDone, name } = req.body
+
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => {
+      todo.name = name
+      todo.isDone = isDone === "on"
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
+
 
 // delete
 
